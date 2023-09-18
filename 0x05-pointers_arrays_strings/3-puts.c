46bab6796e5395a4225bca2adcadb8f4a22b5b79
Ing-gogo <gloriaingabire71@gmail.com>
2023-09-18 03:31:09 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - printing a string
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0 ; str[c] != '\0' ; c++)
+		_putchar(str[c]);
+	_putchar('\n');
+}
