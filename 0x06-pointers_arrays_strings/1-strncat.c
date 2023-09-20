3917e7277c2a95de28cb214fbd88702dce2c2fe3
Ing-gogo <gloriaingabire71@gmail.com>
2023-09-20 11:49:16 -0700

    1-task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat - name of function
+ * @dest: destination
+ * @src:source
+ * @n: integer
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int b;
+
+	for (b = 0 ; b < n && *src != '\0' ; b++)
+	{
+		dest[len + b] = *src;
+		src++;
+	}
+	dest[len + b] = '\0';
+	return (dest);
+}
