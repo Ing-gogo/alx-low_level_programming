d6846d01df67e3e68fc1a9cfb4a0ec1f407ba9f0
Ing-gogo <gloriaingabire71@gmail.com>
2023-09-20 12:10:54 -0700

    2-task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - function name
+ * @dest: destination
+ * @src: source
+ * @n: integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c;
+
+	for (c = 0 ; c < n && src[c] != '\0' ; c++)
+	{
+		while (c < n)
+		{
+			dest[c] = '\0';
+			c++;
+		}
+	}
+	return (dest);
+}
