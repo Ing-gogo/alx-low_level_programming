a033bf0f86470462059c4e7fd25bff2038fae1ae
Ing-gogo <gloriaingabire71@gmail.com>
2023-09-25 08:24:42 -0700

    7-chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j, k;
+
+	for (j = 0; j < 0; j++)
+	{
+		for (k = 0 ; k < 0 ; k++)
+			_putchar(a[j][k]);
+		_putchar('\n');
+	}
+}
